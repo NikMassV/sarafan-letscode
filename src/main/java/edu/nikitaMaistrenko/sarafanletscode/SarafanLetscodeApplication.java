7b2f6be387eeb6384ade7e9d6a798054911303f9
Maistrenko Nikita <slipknoshka777@gmail.com>
2021-05-03 23:29:31 +0300

    Simple REST

---
+++ src/main/java/edu/nikitaMaistrenko/sarafanletscode/SarafanLetscodeApplication.java
@@ -0,0 +1,13 @@
+package edu.nikitaMaistrenko.sarafanletscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarafanLetscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarafanLetscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nikitaMaistrenko/sarafanletscode/SarafanLetscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nikitaMaistrenko.sarafanletscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanLetscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
